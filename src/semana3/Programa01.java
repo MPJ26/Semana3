246eb49f8e952c7feb3add1920c1f41da4cd752a
Milagros Porras <Milagros Porras@DESKTOP-6QL3KMV>
2024-08-27 21:49:04 -0500

    corecto

--- src/semana3/Programa01.java
+++ src/semana3/Programa01.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 public class Programa01 {
     public static void main (String[] args){
         //declarar variables 
-        int n1,n2,n3,n4;
+        int n1,n2,n3,n4,mayor;
         String mensaje;
         Scanner lectura= new Scanner (System.in);
         //entrada de datos
-        System.out.print("Ingresa cuarto número: ");
+        System.out.print("Ingresa el primer número: ");
+        n1=lectura.nextInt();
+        System.out.print("Ingresa el segundo número: ");
+        n2=lectura.nextInt();
+        System.out.print("Ingresa el tercer número: ");
+        n3=lectura.nextInt();        
+        System.out.print("Ingresa el cuarto número: ");
         n4=lectura.nextInt();
         //proceso de datos
         mayor=n1;
